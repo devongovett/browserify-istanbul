99da4e54485426e6e54136c8717508cd889a9fb2
Sylvain Malnuit <sylvain.malnuit@lyra-network.com>
2014-07-04 13:24:51 +0200

    add parameters to define options with karma:coverage

--- index.js
+++ index.js
@@ -6,33 +6,34 @@ var path = require('path');
 var instrumenter = new istanbul.Instrumenter();
 var defaultIgnore = ['**/node_modules/**', '**/test/**', '**/tests/**'];
 
-module.exports = function(options) {
-  options = options || {};
-  
-  function transform(file) {
-    var ignore = options.defaultIgnore === false ? [] : defaultIgnore;
-    ignore = ignore.concat(options.ignore || []);
-    
-    if (ignore.some(minimatch.bind(null, file)))
-      return through();
-    
-    var data = '';
-    return through(function(buf) {
-      data += buf;
-    }, function() {
-      var self = this;
-      instrumenter.instrument(data, file, function(err, code) {
-        self.queue(code);
-        self.queue(null);
-      });
-    });
-  }
-      
-  if (typeof options === 'string') {
-    var file = options;
-    options = {};
-    return transform(file);
-  }
-  
-  return transform;
+module.exports = function (options, opt) {
+	options = options || {};
+	opt = opt || {};
+
+	function transform(file) {
+		var ignore = options.defaultIgnore === false ? [] : defaultIgnore;
+		ignore = ignore.concat(options.ignore || []);
+
+		if (ignore.some(minimatch.bind(null, file)))
+			return through();
+
+		var data = '';
+		return through(function (buf) {
+			data += buf;
+		}, function () {
+			var self = this;
+			instrumenter.instrument(data, file, function (err, code) {
+				self.queue(code);
+				self.queue(null);
+			});
+		});
+	}
+
+	if (typeof options === 'string') {
+		var file = options;
+		options = opt;
+		return transform(file);
+	}
+
+	return transform;
 };
